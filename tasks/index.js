5e9448f17bea9fa3626c8bd65c2aabd7cfdcd2e8
Nicholas Schreiber <rnd7@riseup.net>
2018-10-21 17:11:45 +0200

    Added esm build subtask

--- tasks/index.js
+++ tasks/index.js
@@ -2,6 +2,10 @@ import gulp from 'gulp'
 import path from 'path'
 import process from 'process'
 import webpack from 'webpack'
+import file from 'gulp-file';
+
+import { rollup } from 'rollup';
+import babel from 'rollup-plugin-babel';
 
 const isProduction = true // (process.env.NODE_ENV === 'production')
 
@@ -10,9 +14,9 @@ export const config = {
   entry: './index.js',
   mode: isProduction ? "production" : "development",
   output: {
-    filename: 'rum-storage.js',
+    filename: 'rum-storage-umd.js',
     path: path.resolve(__dirname, '../dist'),
-    library: 'rum-storage',
+    library: '@rnd7/rum-storage',
     libraryTarget: 'umd'
   },
   context: path.resolve(__dirname, '../src'),
@@ -39,12 +43,46 @@ export const config = {
   }
 }
 
-export const build = () => {
+
+export const buildUMD = () => {
   return new Promise(resolve => webpack(config, (err, stats) => {
     if(err) console.log('Webpack', err)
     console.log(stats.toString({ }))
     resolve()
   }))
 }
+export const buildESM = () => {
+  return rollup({
+    input: './src/index.js',
+    output: {
+      file: 'bundle.js',
+      format: 'esm'
+    },
+    plugins: [
+      babel({
+        presets: [
+          [
+            "@babel/preset-env", {
+              "modules": false
+            }
+          ]
+        ],
+        babelrc: false,
+        exclude: 'node_modules/**'
+      })
+    ]
+  })
+  .then(bundle => {
+    return bundle.generate({
+      moduleName: '@rnd7/rum-storage',
+      format: 'esm'
+    })
+  })
+  .then(gen => {
+    return file('rum-storage-esm.js', gen.code, {src: true})
+      .pipe(gulp.dest('dist/'))
+  })
 
-export default build
+}
+
+export default gulp.parallel(buildUMD, buildESM)
